4433fdac1329681c9f28e33b7eab70afedeffac1
RyanDrx <Ryan.Dierickx@gmail.com>
2017-10-04 11:57:35 -0700

    Add create user UI

--- app/src/main/java/com/ryandrx/smack/LoginActivity.kt
+++ app/src/main/java/com/ryandrx/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.ryandrx.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserButtonClicked(view: View){
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 
