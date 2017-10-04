129314116e10266497f8adef11cef7920a850662
RyanDrx <Ryan.Dierickx@gmail.com>
2017-10-04 10:15:49 -0700

    Add main ui components

--- app/src/main/java/com/ryandrx/smack/MainActivity.kt
+++ app/src/main/java/com/ryandrx/smack/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.ryandrx.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -30,5 +27,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginButtonNavClicked(view: View) {
+
+    }
+
+    fun addChannelButtonClicked(view: View) {
+
+    }
+
+    fun sendMessageButtonClicked(view: View) {
+
     }
 }
